45580b8b17203e1c18587d194e714827f27e5abc
martip23 <patrickmartinez77@gmail.com>
2017-09-22 01:43:59 -0500

    Nightly build

--- src/binarySearchTree/BinTree.java
+++ src/binarySearchTree/BinTree.java
@@ -1,19 +1,54 @@
 package binarySearchTree;
 
-public class BinTree {
+public class BinTree {	
+	
+	private Node root;
 	
 	BinTree() {
-		// TODO BinTree Constructor
 		System.out.println("BinTree created");
 	}
 	
-	public void insertNode(Node node) {
-		// TODO insertNode Stub
-		System.out.println("insertNode called");
+	public void insertNode(int num) {
+		insertNode(root, num);
 	}
 	
-	public void treeWalk() {
-		// TODO treeWalk Stub
-		System.out.println("treeWalk called");
+	public Node insertNode(Node node) {
+		if (root == null) {
+			root = node;
+		}
+		else if (node.getKey() < root.getKey()) {
+			node.setLeftChild(insertNode(root.getLeftChild()));
+		}
+		else {
+			node.setRightChild(insertNode(root.getRightChild()));
+		}
+		return node;
+	}
+	
+	private Node insertNode(Node node, int num) {
+		// TODO insertNode Stub	
+		if (root == null) {
+			root = new Node(num);
+		} 
+		else if (num < root.getKey()){
+			insertNode(root.getLeftChild(), num);
+		} 
+		else {
+			insertNode(root.getRightChild(), num);
+		}
+		return node;
+	}
+	
+	public void treeWalk () {
+		treeWalk(root);
+	}
+	
+	private void treeWalk(Node root) {
+
+		if (root != null) {
+			treeWalk(root.getLeftChild());
+			System.out.println(root.getKey() + "-");
+			treeWalk(root.getRightChild());
+		}
 	}
 }
--- src/binarySearchTree/Node.java
+++ src/binarySearchTree/Node.java
@@ -2,7 +2,39 @@ package binarySearchTree;
 
 public class Node {
 	
+	private int key;
+	private Node leftChild;
+	private Node rightChild;
+	
 	Node(int val) {
-		System.out.println("Node created with value: " + val);
+		key = val;
 	}
+	
+	
+	/* SETTERS */
+	public void setKey(int val) {
+		key = val;
+	}
+	
+	public void setLeftChild(Node node) {
+		leftChild = node;
+	}
+	
+	public void setRightChild(Node node) {
+		rightChild = node;
+	}
+	
+	/* GETTERS */
+	public int getKey() {
+		return key;
+	}
+	
+	public Node getLeftChild() {
+		return leftChild;
+	}
+	
+	public Node getRightChild() {
+		return rightChild;
+	}
+	
 }
